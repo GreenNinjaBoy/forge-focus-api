1b442eacf4e3d6116149075f57cb2fb668b73e3e
jamie connell <Jamie.Connell_10@hotmail.com>
2024-05-16 20:20:34 +0000

    refine(views.py, create RefineList class)

--- refine/views.py
+++ refine/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from forge_focus_api.permissions import OwnerOnly
+from .serializers import RefineSerializer
+from .models import Refine
+from rest_framework import generics
 
-# Create your views here.
+
+class RefineList(generics.ListCreateAPIView):
+    """
+     This view will return a list of refined area for the
+     logged in user, will also a new area for refinement.
+    """
+    serializer_class = RefineSerializer
+
+    def refinement_create(self, serializer):
+        """
+        This will add the owner data to the refine object
+        before it is saved.
+        """
+        serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        """
+        This will pull all the refine instances that are only linked
+        to the current user. Within this order will be rank first 
+        (with null last), and then created_at.
+        """
+        return self.request.user.focus.all().order_by('rank', 'created_at')
+
+
+
