26ad57a4c2db1002947847b8f59a161a6b79fe09
jamie connell <Jamie.Connell_10@hotmail.com>
2024-05-21 08:53:12 +0000

    goals(views.py - add imports for file)

--- goals/views.py
+++ goals/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from forge_focus.permissions import OwnerOnly
+from .models import UserGoals
+from .serializers import UserGoalsSerializer
+from rest_framework import generics, filters
 
-# Create your views here.
+
