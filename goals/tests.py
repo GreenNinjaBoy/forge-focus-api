a7d8eefce463638735012d0cc8ab37a2fb66dd20
jamie connell <Jamie.Connell_10@hotmail.com>
2024-05-22 08:17:30 +0000

    goals(tests.py - add imports for tests)

--- goals/tests.py
+++ goals/tests.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import UserGoals
+from refine.models import Refine
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
