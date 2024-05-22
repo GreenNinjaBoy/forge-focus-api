674b188ca57b267bbfb38bfba13ca739a9f934a8
jamie connell <Jamie.Connell_10@hotmail.com>
2024-05-22 07:08:52 +0000

    goals(views.py - add first view)

--- goals/views.py
+++ goals/views.py
@@ -3,4 +3,24 @@ from .models import UserGoals
 from .serializers import UserGoalsSerializer
 from rest_framework import generics, filters
 
+class FilterList(filters.BaseFilterBackend):
+    """
+    This is a custom filter which will 
+    allow the user to filter their goals
+    by refine_id, no parent and parent_id
+    """
+
+    def filter_queryset(self, request, queryset, view):
+        refine_id = request.query_params.get('refine_id')
+        parent_id = request.query_params.get('parent_id')
+        parent = request.query_params.get('parent')
+        if refine_id:
+            queryset = queryset.filter(refine_id=refine_id)
+        if parent_id:
+            queryset = queryset.filter(parent_id=parent_id)
+        if parent:
+            queryset = queryset.filter(parent=None)
+        return queryset
+
+
 
